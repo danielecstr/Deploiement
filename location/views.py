2a0b5d1bc1a4eb6dcf8ca6244e56c10743203eb7
danielecstr <daniele.cstr@eduge.ch>
2021-10-13 15:37:07 +0200

    .

--- location/views.py
+++ location/views.py
@@ -41,7 +41,7 @@ def location(request):
             listeMail.append(loc.lv_loc_id.loc_client.cli_mail)
 
 
-    #send_mail('La bicycletteBleue', 'Bonjour, nous vous envoyons cet email pour vous prévenir que votre location se termine dans 10 jours.', settings.EMAIL_HOST_USER, listeMail, fail_silently=False)
+    send_mail('La bicycletteBleue', 'Bonjour, nous vous envoyons cet email pour vous prévenir que votre location se termine dans 10 jours.', settings.EMAIL_HOST_USER, listeMail, fail_silently=False)
 
     locations = []
     for loc in locationvelo:
